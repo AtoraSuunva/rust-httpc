aacc368ce72e51cb8b1b8dd12eb31e7afefaa4f0
AtoraSuunva <atora@giraffeduck.com>
2022-09-27 02:51:47 -0400

    chore(cli): better cli help docs

--- src/cli.rs
+++ src/cli.rs
@@ -7,8 +7,15 @@ use clap::{ArgGroup, Parser, Subcommand, ValueHint};
 #[derive(Debug, Parser)]
 #[clap(name = "httpc")]
 #[clap(version = "1.0")]
-#[clap(about = "HTTP client", long_about = "long http client")]
+#[clap(
+    about = "HTTP client",
+    long_about = "httpc is a curl-like application but supports HTTP protocol only"
+)]
 pub struct Cli {
+    /// Get help for this command.
+    #[clap(long)]
+    help: bool,
+
     #[clap(subcommand)]
     pub command: Commands,
 }
@@ -16,22 +23,22 @@ pub struct Cli {
 #[derive(Debug, Subcommand)]
 #[clap(group(ArgGroup::new("body")))]
 pub enum Commands {
-    /// Executes a HTTP GET request and prints the response.
+    /// Executes an HTTP GET request and prints the response.
     Get {
         #[clap(flatten)]
         options: CommonOptions,
     },
 
-    /// Executes a HTTP POST request and prints the response.
+    /// Executes an HTTP POST request and prints the response.
     Post {
         #[clap(flatten)]
         options: CommonOptions,
 
-        /// string Associates an inline data to the body HTTP POST request.
+        /// Associates an inline data to the body HTTP POST request.
         #[clap(short, group = "body", value_parser)]
         data: Option<String>,
 
-        /// file Associates the content of a file to the body HTTP POST request.
+        /// Associates the content of a file to the body HTTP POST request.
         #[clap(short, group = "body", value_parser, value_hint = ValueHint::FilePath)]
         file: Option<String>,
     },
@@ -39,24 +46,20 @@ pub enum Commands {
 
 #[derive(Debug, Parser)]
 pub struct CommonOptions {
-    /// Get help for this command.
-    #[clap(long)]
-    help: bool,
-
     /// Prints the detail of the response such as protocol, status, and headers.
     #[clap(short, value_parser)]
     pub verbose: bool,
 
     /// Output to a file instead of stdout
-    #[clap(short, value_parser, value_hint = ValueHint::FilePath)]
+    #[clap(short, value_parser, value_name = "FILE", value_hint = ValueHint::FilePath)]
     pub output: Option<String>,
 
     /// Follow 'Location' header redirects by repeating requests
     #[clap(short, value_parser)]
     pub location: bool,
 
-    /// key:value Associates headers to HTTP Request with the format 'key:value'.
-    #[clap(short, value_parser)]
+    /// Associates headers to HTTP Request with the format 'key:value'.
+    #[clap(short, value_parser, value_name = "key:value")]
     pub header: Vec<String>,
 
     /// URL to send the request to.
